9ce1e91087f446d5295a6f8123e76b88476bced3
juanmann1 <43592682+juanmann1@users.noreply.github.com>
2021-12-27 15:03:24 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Proyecto Sublimados Tin Tin.
+          Proyecto Sublimados Tin Tin
         </p>
         <a
           className="App-link"
